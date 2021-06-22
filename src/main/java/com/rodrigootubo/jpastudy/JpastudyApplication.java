cab8702e2dc5840110a4d3c8d1f021f2844baabe
Rodrigo Otubo <rodrigo.otubo@gmail.com>
2021-06-21 21:59:40 -0300

    Initial project

---
+++ src/main/java/com/rodrigootubo/jpastudy/JpastudyApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigootubo.jpastudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpastudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpastudyApplication.class, args);
+	}
+
+}
